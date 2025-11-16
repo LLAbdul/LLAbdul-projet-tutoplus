4c53e6a7d83ff986f276dc7e452262e36c2d1bd9
Abdul Rahman Zahid <202027024@collegeahuntsic.qc.ca>
2025-11-16 01:02:16 -0500

    Ajout d'icônes SVG pour le lien de déconnexion dans gestion_disponibilites.php et ajout d'un bouton de déconnexion rapide.

--- gestion_disponibilites.php
+++ gestion_disponibilites.php
@@ -88,7 +88,13 @@ $cacheBuster = time();
                     <div class="burger-menu-desktop" id="burgerMenuDesktop">
                         <a href="gestion_disponibilites.php" class="burger-menu-link">Mes Disponibilités</a>
                         <a href="gestion_demandes.php" class="burger-menu-link">Mes Demandes</a>
-                        <a href="logout.php" class="burger-menu-link burger-menu-link-logout">Déconnexion</a>
+                        <a href="logout.php" class="burger-menu-link burger-menu-link-logout">
+                            <span>Déconnexion</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+                                <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+                            </svg>
+                        </a>
                     </div>
                     <div class="user-info">
                         <span class="user-name">
@@ -98,6 +104,12 @@ $cacheBuster = time();
                             <?= htmlspecialchars($_SESSION['tuteur_numero'], ENT_QUOTES, 'UTF-8') ?>
                         </span>
                     </div>
+                    <a href="logout.php" class="btn-logout-quick" aria-label="Déconnexion" title="Déconnexion">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+                            <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+                        </svg>
+                    </a>
 
                     <!-- Menu dropdown pour mobile -->
                     <div class="user-menu-dropdown">
